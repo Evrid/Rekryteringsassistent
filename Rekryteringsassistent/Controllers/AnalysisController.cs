6162043bb951db40cb4a2de36deefb7ddc52119b
Kevin li <talentsgenius@outlook.com>
2023-10-10 09:49:00 +0200

    Changed controller to cshtml.cs

--- Rekryteringsassistent/Controllers/AnalysisController.cs
+++ Rekryteringsassistent/Controllers/AnalysisController.cs
@@ -1,35 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Rekryteringsassistent.Models;
-using Rekryteringsassistent.Services;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Rekryteringsassistent.Models;
+//using Rekryteringsassistent.Services;
 
-namespace Rekryteringsassistent.Controllers
-{
-    public class AnalysisController : Controller
-    {
-        private readonly AIAnalysisService _aiAnalysisService;
+//namespace Rekryteringsassistent.Controllers
+//{
+//    public class AnalysisController : Controller
+//    {
+//        private readonly AIAnalysisService _aiAnalysisService;
 
-        public AnalysisController(AIAnalysisService aiAnalysisService)
-        {
-            _aiAnalysisService = aiAnalysisService;
-        }
+//        public AnalysisController(AIAnalysisService aiAnalysisService)
+//        {
+//            _aiAnalysisService = aiAnalysisService;
+//        }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
 
-        [HttpPost]
-        public async Task<IActionResult> Index(Candidate candidate, Criteria criteria) // Or use ViewModel that includes both Candidate and Criteria
-        {
-            if (ModelState.IsValid)
-            {
-                var analysisResult = await _aiAnalysisService.AnalyzeCandidate(candidate, criteria);
+//        [HttpPost]
+//        public async Task<IActionResult> Index(Candidate candidate, Criteria criteria) // Or use ViewModel that includes both Candidate and Criteria
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                var analysisResult = await _aiAnalysisService.AnalyzeCandidate(candidate, criteria);
 
-                // Store the analysis result in ViewBag to display it
-                ViewBag.AnalysisResult = analysisResult;
-            }
-            return View(candidate);
-        }
-    }
+//                // Store the analysis result in ViewBag to display it
+//                ViewBag.AnalysisResult = analysisResult;
+//            }
+//            return View(candidate);
+//        }
+//    }
 
-}
+//}
--- Rekryteringsassistent/Pages/Analysis.cshtml.cs
+++ Rekryteringsassistent/Pages/Analysis.cshtml.cs
@@ -1,12 +1,45 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rekryteringsassistent.Models;
+using Rekryteringsassistent.Services;
 
 namespace Rekryteringsassistent.Pages
 {
     public class AnalysisModel : PageModel
     {
+        private readonly AIAnalysisService _aiAnalysisService;
+
+        public AnalysisModel(AIAnalysisService aiAnalysisService)
+        {
+            _aiAnalysisService = aiAnalysisService;
+        }
+
+        [BindProperty]
+        public Candidate Candidate { get; set; }
+
+        [BindProperty]
+        public Criteria Criteria { get; set; }
+
+        public AIAnalysisResult AnalysisResult { get; set; }
+
         public void OnGet()
         {
+            // Initialize Candidate and Criteria if you want
+            Candidate = new Candidate();
+            Criteria = new Criteria();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            AnalysisResult = await _aiAnalysisService.AnalyzeCandidate(Candidate, Criteria);
+
+            return Page();
         }
     }
 }
